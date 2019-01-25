a317b44a93d4a3b607082d6cc82822874f328af1
APCS2nd <APCS2nd@MHS-Dreyer-10>
2019-01-25 09:36:51 -0800

    Undone

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -36,84 +36,10 @@ public class FracCalc {
 	    
 	    
 	    
-	    int[] firstOperand = new int [3];
-	    int[] secondOperand = new int [3];
-	    //checkpoint2, which is split the first and second operand and store them in two integer arrays.
-	    secondOperand = checkpoint2(threeParts[2],secondOperand);
-	    firstOperand = checkpoint2(threeParts[0],firstOperand);
-	    // convert to improper fraction
-	    toImproper(firstOperand);
-	    toImproper(secondOperand);  
+	  
 	    
 	    int[] result = new int [3];
-	    //calculations
-	    if (threeParts[1].equals("+")) {
-	    	toCommonDenominator(firstOperand, secondOperand);
-	      	result[1] = firstOperand[1] + secondOperand[1];
-	    	result[2] = firstOperand[2];
-	    }
-	    else if(threeParts[1].equals("-")) {
-	    	toCommonDenominator(firstOperand, secondOperand);
-	    	result[1] = firstOperand[1] - secondOperand[1];
-	    	result[2] = firstOperand[2];
-	
-	    }
-	    else if(threeParts[1].equals("*")) {
-	    	result[1] = firstOperand[1] * secondOperand[1];
-	    	result[2] = firstOperand[2] * secondOperand[2];
-	    } 
-	    else {
-	    	result[1] = firstOperand[1] * secondOperand[2];
-	    	result[2] = firstOperand[2] * secondOperand[1];
-	    	
-	    }
-	    
-	
-	
-		// reduce fraction
-	    if (result[1]<0 &&result[2]<0) {
-	    	result[1] = -result[1];
-	    	result[2] = -result[2];
-	    }
-	    int i = 0;
-	    while (i != 1) {
-	    	i = greatestCommonFactor(result[1], result[2]);
-	    	result[1] = result[1]/i;
-	    	result[2] = result[2]/i;
-	    }
-	    
-	    result[0] = result[1]/result[2];
-	    result[1] = result[1]%result[2];
-	    //change the sign of result fraction part if it's negative.
 	   
-	  
-	    if (result[1] <0) {
-	    	if (result[0]<0) {
-	        	result[1] = -result[1];
-	        	
-	        } 
-	    	else if (result[0]>0){
-	    		result[0] = -result[0];
-	    		result[1] = -result[1];
-	    	}
-	   
-	    	}
-	    
-	    else if(result[2]<0) {
-	    	if (result[0]<0) {
-	        	result[2] = -result[2];
-	        	
-	        }
-	    	else if (result[0]>0){
-	    		result[0] = -result[0];
-	    		result[2] = -result[2];
-	    	}
-	    	else if (result[0]==0 && result[1]>0) {
-	    		result[1]=-result[1];
-	    		result[2]=-result[2];
-	    	}
-	    }
-	    
 	    //return the answer
 	    if (result[1] == 0) {
 	    	 return (result[0] +"");
--- FracCalc/src/fracCalc/Fraction.java
+++ FracCalc/src/fracCalc/Fraction.java
@@ -9,15 +9,122 @@ public class Fraction {
 	
 	
 	//Constructors
-	public Fraction(String operand) {
-	}
+	public Fraction(String input) {
+		//convert the String operand into three integers that store in an array called operandInt.
+		        int indexOf_ = input.indexOf('_');
+		        int indexOfslash = input.indexOf('/');
+		        if (indexOf_ == -1 && indexOfslash != -1) {     // 2/5
+		        	operandInt[0] = 0;
+		        	operandInt[1] = Integer.parseInt(input.substring(0,indexOfslash));
+		        	operandInt[2] = Integer.parseInt(input.substring(indexOfslash+1));
+		        }
+		        else if(indexOf_ == -1 && indexOfslash == -1) {   //3
+		        	operandInt[0] = Integer.parseInt(input);
+		        	operandInt[1] = 0;
+		        	operandInt[2] = 1;
+		        }
+		        else {                      //3_2/5
+		        	String wholeNumberString2 = input.substring(0,indexOf_);
+		        	int wholeNumberInt2 = Integer.parseInt(wholeNumberString2);
+		        	String fractionString = input.substring(indexOf_+1);
+		        	operandInt[0] = wholeNumberInt2;
+		        	indexOfslash = fractionString.indexOf('/');
+		        	operandInt[1] = Integer.parseInt(fractionString.substring(0,indexOfslash));
+		            operandInt[2] = Integer.parseInt(fractionString.substring(indexOfslash+1));
+		        }
+		        
+		        //convert the fraction to improper
+		       
+			    	int wholeNumber = operandInt[0];
+			    	if (wholeNumber < 0) {
+			    		operandInt[1] = -operandInt[1] + operandInt[2]*operandInt[0];
+			    	}
+			    	else {
+			    		operandInt[1] = operandInt[1] + operandInt[2]*operandInt[0];
+			    	}
+			    	operandInt[0] = 0;
+			    }
+		        
+		    
 	
 	public Fraction() {
+		operandInt[0] = 0;
+    	operandInt[1] = 0;
+    	operandInt[2] = 1;
 	}
 	
 	//Methods
 	
-	public Fraction doMath(char operator, String operand) {
+	public Fraction doMath(char operator, Fraction operand) {
+		 //calculations
+		Fraction result = new Fraction();
+	    if (operator =='+') {
+	    	toCommonDenominator(firstOperand, secondOperand);
+	      	result[1] = firstOperand[1] + secondOperand[1];
+	    	result[2] = firstOperand[2];
+	    }
+	    else if(operator =='-')) {
+	    	toCommonDenominator(firstOperand, secondOperand);
+	    	result[1] = firstOperand[1] - secondOperand[1];
+	    	result[2] = firstOperand[2];
+	
+	    }
+	    else if(operator =='*') {
+	    	result[1] = firstOperand[1] * secondOperand[1];
+	    	result[2] = firstOperand[2] * secondOperand[2];
+	    } 
+	    else {
+	    	result[1] = firstOperand[1] * secondOperand[2];
+	    	result[2] = firstOperand[2] * secondOperand[1];
+	    	
+	    }
+	    
+	
+	
+		// reduce fraction
+	    if (result[1]<0 &&result[2]<0) {
+	    	result[1] = -result[1];
+	    	result[2] = -result[2];
+	    }
+	    int i = 0;
+	    while (i != 1) {
+	    	i = greatestCommonFactor(result[1], result[2]);
+	    	result[1] = result[1]/i;
+	    	result[2] = result[2]/i;
+	    }
+	    
+	    result[0] = result[1]/result[2];
+	    result[1] = result[1]%result[2];
+	    //change the sign of result fraction part if it's negative.
+	   
+	  
+	    if (result[1] <0) {
+	    	if (result[0]<0) {
+	        	result[1] = -result[1];
+	        	
+	        } 
+	    	else if (result[0]>0){
+	    		result[0] = -result[0];
+	    		result[1] = -result[1];
+	    	}
+	   
+	    	}
+	    
+	    else if(result[2]<0) {
+	    	if (result[0]<0) {
+	        	result[2] = -result[2];
+	        	
+	        }
+	    	else if (result[0]>0){
+	    		result[0] = -result[0];
+	    		result[2] = -result[2];
+	    	}
+	    	else if (result[0]==0 && result[1]>0) {
+	    		result[1]=-result[1];
+	    		result[2]=-result[2];
+	    	}
+	    }
+	    
 		
 	}
 	public String toString() {
@@ -50,41 +157,8 @@ public class Fraction {
 			return result;
 	    }
 	    
-	    public static int[] checkpoint2 (String input, int [] operand) {
-	        int indexOf_ = input.indexOf('_');
-	        int indexOfslash = input.indexOf('/');
-	        if (indexOf_ == -1 && indexOfslash != -1) {     // 2/5
-	        	operand[0] = 0;
-	        	operand[1] = Integer.parseInt(input.substring(0,indexOfslash));
-	        	operand[2] = Integer.parseInt(input.substring(indexOfslash+1));
-	        }
-	        else if(indexOf_ == -1 && indexOfslash == -1) {   //3
-	        	operand[0] = Integer.parseInt(input);
-	        	operand[1] = 0;
-	        	operand[2] = 1;
-	        }
-	        else {                      //3_2/5
-	        	String wholeNumberString2 = input.substring(0,indexOf_);
-	        	int wholeNumberInt2 = Integer.parseInt(wholeNumberString2);
-	        	String fractionString = input.substring(indexOf_+1);
-	        	operand[0] = wholeNumberInt2;
-	        	indexOfslash = fractionString.indexOf('/');
-	        	operand[1] = Integer.parseInt(fractionString.substring(0,indexOfslash));
-	            operand[2] = Integer.parseInt(fractionString.substring(indexOfslash+1));
-	        }
-	        return operand;  
-	    }
-
-	    public static void toImproper(int[] fraction) {
-	    	int wholeNumber = fraction[0];
-	    	if (wholeNumber < 0) {
-	    		fraction[1] = -fraction[1] + fraction[2]*fraction[0];
-	    	}
-	    	else {
-	    		fraction[1] = fraction[1] + fraction[2]*fraction[0];
-	    	}
-	    	fraction[0] = 0;
-	    }
+	   
+	   
 	    
 	    public static boolean isDivisibleBy(int number1,int number2) {
 			if (number2 == 0) {
