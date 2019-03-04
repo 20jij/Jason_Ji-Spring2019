f8ed4dc78225f21ef4da9b7fc571114fcea62c5c
APCS2nd <APCS2nd@MHS-Dreyer-10>
2019-03-04 09:50:58 -0800

    Text Excel initialization.

---
+++ TextExcel/src/textExcel/EmptyCell.java
@@ -0,0 +1,23 @@
+//Jason Ji
+//3/4/2019
+package textExcel;
+
+public class EmptyCell implements Cell {
+
+	// constructor
+	public EmptyCell() {
+		
+	}
+	
+	// text for spreadsheet cell display, must be exactly length 10
+	public String abbreviatedCellText() {
+		
+	} 
+	
+	
+	// text for individual cell inspection, not truncated or padded
+	public String fullCellText() {
+		
+	}
+
+}
