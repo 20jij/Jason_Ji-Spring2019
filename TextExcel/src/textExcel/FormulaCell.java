e363e2b1aa2fee1e76561e9abf8a8b5bacedd77a
APCS2nd <APCS2nd@MHS-Dreyer-10>
2019-03-29 09:18:04 -0700

    Finished Checkpoint4

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -15,14 +15,37 @@ public class FormulaCell extends RealCell {
 	}
 	
 	public String abbreviatedCellText() {
-		String temp = super.getValueText() + "          ";
+		String temp = getDoubleValue() + "          ";
 		return temp.substring(0, 10);
 		
 	}
 	public double getDoubleValue() {
 		String formula = super.getValueText();
-		for 
-		return 0.0;
+		String[]  splitFormula = formula.split(" ");
+		double answer;
+		answer =  Double.parseDouble(splitFormula[1]);
+		for(int i =3; i<splitFormula.length-1;i=i+2) {
+			//i is the index for doubles 
+			//k is the index for signs
+			int k = i -1;
+			double temp = Double.parseDouble(splitFormula[i]);
+			if ( splitFormula[k].equals("+")) {
+				answer = answer + temp ;
+			}
+			
+			else if(splitFormula[k].equals("-")) {
+				answer = answer - temp;
+			}
+			else if(splitFormula[k].equals("*")) {
+				answer = answer * temp;
+			}
+			else{
+				answer = answer / temp;
+			}
+		}
+		
+		
+		return answer;
 	}
 
 	
